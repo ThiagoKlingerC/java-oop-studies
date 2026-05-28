05658c8a4ff17038ea289821a742061cd8b6acd2
ThiagoKlingerC <thiagoklinger@hotmail.com>
2026-05-27 21:20:26 -0300

    refactor: add generic catch block

--- 07-exceptions-handling/ex01-custom-exceptions-hotel/src/application/Program.java
+++ 07-exceptions-handling/ex01-custom-exceptions-hotel/src/application/Program.java
@@ -43,6 +43,9 @@ public class Program {
 		catch(DomainException e) {
 			System.out.println(e.getMessage());
 		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
 		sc.close();
 	
 		}
